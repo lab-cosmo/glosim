edf53ce3a702b01518b59a13c83a229f20423541
Michele Ceriotti <michele.ceriotti@gmail.com>
2015-09-23 07:45:08 +0200

    Cleared up a circular import by separating environments and structures. Seemingly also fixed permanent kernel

---
+++ libmatch/environments.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# Computes the matrix of similarities between structures in a xyz file
+# by first getting SOAP descriptors for all environments, finding the best
+# match between environments using the Hungarian algorithm, and finally
+# summing up the environment distances.
+# Supports periodic systems, matching between structures with different
+# atom number and kinds, and sports the infrastructure for introducing an
+# alchemical similarity kernel to match different atomic species
+
+import sys, time
+from copy import copy, deepcopy
+import numpy as np
+
+__all__ = [ "environ", "alchemy", "envk" ]
+
+class alchemy:
+   def getpair(self, sa, sb):
+      if len(self.rules)==0: # special case when the alchemical matrix is default
+         if sa==sb: return 1
+         else: return 0  
+      else:
+          if sa<=sb and (sa,sb) in self.rules:            
+             return self.rules[(sa,sb)]
+          elif sa>sb and (sb,sa) in self.rules:
+             return self.rules[(sb,sa)] 
+          else: 
+             if sa==sb: return 1
+             else: return 0  
+   
+   def __init__(self, rules={}, mu=0):            
+      self.rules = rules.copy()
+      self.mu = mu
+      
+
+class environ:   
+   def getpair(self, sa, sb):
+      # siab = sidx(sa, sb)
+      siab = (sa,sb) # the power spectra are not fully symmetric with respect to exchange of species index, unless one also exchanges n1 and n2, which is a mess.
+      if siab in self.soaps:               
+         return self.soaps[siab]
+      else: 
+         if len(self.soaps)==0: # dummy atoms environments just returned as isolated species!            
+            if sa==sb and sa==self.z: return self.dummy1
+         return self.dummy0  
+         
+   def __init__(self, nmax, lmax, salchem=None, specie=0):            
+      self.alchem = salchem
+      if self.alchem is None: self.alchem=alchemy()
+      
+      self.z = specie
+      
+      self.nmax = nmax
+      self.lmax = lmax      
+      self.dummy0 = np.zeros((self.nmax*self.nmax)*(self.lmax+1), float); 
+      self.dummy1 = self.dummy0.copy(); self.dummy1[0]=1.0;
+      
+      self.soaps = {}        
+      if self.z == 0:
+        self.nspecies = 0
+        self.zspecies = []
+        self.natoms = 0
+      else:
+        self.nspecies = 1
+        self.zspecies = [self.z]
+        self.natoms = 1
+      
+   
+   def add(self, nenv):
+      # combine the SOAPS in the nenv environment to this one
+      self.zspecies = sorted(list(set(self.zspecies+nenv.zspecies)))  # merges the list of environment species
+      self.nspecies = len(self.zspecies)
+      if self.z>0 and self.z!= nenv.z: self.z=-1  # self species is not defined for a sum of different centers of environments
+      if self.nmax != nenv.nmax or self.lmax != nenv.lmax: raise ValueError("Cannot combine environments with different expansion channels")
+      for k,w in nenv.soaps.items():
+         if k in self.soaps:
+            self.soaps[k] = self.soaps[k]*self.natoms + nenv.soaps[k]*nenv.natoms
+         else:
+            self.soaps[k] = w.copy() * nenv.natoms
+        # print k, self.soaps[k] 
+      self.natoms += nenv.natoms      
+      self.normalize()
+
+   def normalize(self):       
+      nrm = np.sqrt( envk(self, self, self.alchem) )
+      for sij in self.soaps:  self.soaps[sij]*=1.0/nrm
+   
+   def convert(self, specie, species, rawsoap):      
+      self.z = specie
+      self.zspecies = sorted(species)
+      self.nspecies = len(species)
+      self.natoms = 1
+      
+      self.soaps = {}
+      ipair = {}
+      for s1 in range(self.nspecies):
+         for s2 in range(self.nspecies): #  range(s1+1): we actually need to store also the reverse pairs if we want to go alchemical
+            self.soaps[(self.zspecies[s2],self.zspecies[s1])] = np.zeros((self.nmax*self.nmax)*(self.lmax+1), float) 
+            ipair[(self.zspecies[s2],self.zspecies[s1])] = 0
+      
+      isoap = 0
+      isqrttwo = 1.0/np.sqrt(2.0)
+      for s1 in xrange(self.nspecies):
+         for n1 in xrange(self.nmax):         
+            for s2 in xrange(s1+1):
+               selpair = self.soaps[(self.zspecies[s2],self.zspecies[s1])]
+               # we need to reconstruct the spectrum for the inverse species order, that also swaps n1 and n2. 
+               # This is again only needed to enable alchemical combination of e.g. alpha-beta and beta-alpha. Shit happens
+               revpair = self.soaps[(self.zspecies[s1],self.zspecies[s2])]                  
+               isel = ipair[(self.zspecies[s2],self.zspecies[s1])]
+               for n2 in xrange(self.nmax if s2<s1 else n1+1): 
+                  for l in xrange(self.lmax+1):
+                     #print s1, s2, n1, n2, isel, l+(self.lmax+1)*(n2+self.nmax*n1), l+(self.lmax+1)*(n1+self.nmax*n2)            
+                     #selpair[isel] = rawsoap[isoap] 
+                     if (s1 != s2):
+                        selpair[isel] = rawsoap[isoap] * isqrttwo  # undo the normalization since we will actually sum over all pairs in all directions!                        
+                        revpair[l+(self.lmax+1)*(n1+self.nmax*n2)] = selpair[isel]
+                     else: 
+                        # diagonal species (s1=s2) have only half of the elements.          
+                        # this is tricky. we need to duplicate diagonal blocks "repairing" these to be full.
+                        # this is necessary to enable alchemical similarity matching, where we need to combine 
+                        # alpha-alpha and alpha-beta environment fingerprints
+                        selpair[l+(self.lmax+1)*(n2+self.nmax*n1)] = selpair[l+(self.lmax+1)*(n1+self.nmax*n2)] = rawsoap[isoap] * (1 if n1==n2 else isqrttwo)                                             
+                     isoap+=1
+                     isel+=1
+               ipair[(self.zspecies[s2],self.zspecies[s1])] = isel
+      
+      # alchemy-aware normalization
+      self.normalize()
+      
+# SOAP kernel between environments (with possibly alchemical similarity matrix)
+def envk(envA, envB, alchem=alchemy()):
+   dotp = 0.0
+   
+   #union of atom kinds present in the two environments
+   zspecies = sorted(list(set(envA.zspecies+envB.zspecies)))
+      
+   nsp = len(zspecies)
+   
+   if len(alchem.rules) == 0 : # special case, only sum over diagonal bits
+       for s1 in zspecies:
+           for s2 in zspecies:
+               ndot = np.dot(envA.getpair(s1,s2), envB.getpair(s1,s2))
+               dotp+=ndot
+   else:
+       # alchemical matrix for species
+       alchemAB = np.zeros((nsp,nsp), float)
+       for sA in xrange(nsp):
+           for sB in xrange(sA+1):
+               alchemAB[sA,sB] = alchem.getpair(zspecies[sB],zspecies[sA])
+               alchemAB[sB,sA] = alchemAB[sA,sB]       
+       for iA1 in xrange(nsp):
+          sA1 = zspecies[iA1]    
+          for iB1 in xrange(nsp):          
+             if alchemAB[iA1,iB1] == 0.0: continue
+             sB1 = zspecies[iB1]
+             for iA2 in xrange(nsp):
+                sA2 = zspecies[iA2] 
+                for iB2 in xrange(nsp):                              
+                   if alchemAB[iA2,iB2] == 0.0: continue
+                   sB2 = zspecies[iB2]
+                   ndot = np.dot(envA.getpair(sA1,sA2), envB.getpair(sB1,sB2)) * alchemAB[iA1,iB1] * alchemAB[iA2,iB2]
+                   dotp += ndot   
+   #! SHOULD CHECK IF THE ALCHEMY IN THE ENVIRONMENTS IS THE SAME AS THIS, TO RENORMALIZE IF NEEDED
+   return dotp
---
+++ libmatch/structures.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+# Computes the matrix of similarities between structures in a xyz file
+# by first getting SOAP descriptors for all environments, finding the best
+# match between environments using the Hungarian algorithm, and finally
+# summing up the environment distances.
+# Supports periodic systems, matching between structures with different
+# atom number and kinds, and sports the infrastructure for introducing an
+# alchemical similarity kernel to match different atomic species
+
+import sys
+import quippy
+from lap.lap import best_pairs
+import numpy as np
+from environments import environ, alchemy, envk
+__all__ = [ "structk", "gstructk", "structure" ]
+
+   
+class structure:
+   def __init__(self, salchem=None):
+      self.env={}
+      self.species={}
+      self.zspecies = []
+      self.nenv=0  
+      self.alchem=salchem
+      if self.alchem is None: self.alchem=alchemy()
+      self.globenv = None
+      
+   def getnz(self, sp):
+      if sp in self.species:
+         return self.species[sp]
+      else: return 0
+      
+   def getenv(self, sp, i):
+      if sp in self.env and i<len(self.env[sp]):
+         return self.env[sp][i]
+      else: 
+         return environ(self.nmax,self.lmax,self.alchem,sp)  # missing atoms environments just returned as isolated species!
+         
+   def ismissing(self, sp, i):
+      if sp in self.species and i<self.species[sp]:
+         return False
+      else: return True
+   
+      
+   def parse(self, fat, coff=5.0, nmax=4, lmax=3, gs=0.5, cw=1.0, nocenter=[], noatom=[], kit=None):
+      """ Takes a frame in the QUIPPY format and computes a list of its environments. """
+      
+      # removes atoms that are to be ignored
+      at = fat.copy()
+      nol = []
+      for s in range(1,at.z.size+1):
+         if at.z[s] in noatom: nol.append(s)
+      if len(nol)>0: at.remove_atoms(nol)
+      
+      self.nmax = nmax
+      self.lmax = lmax
+      self.species = {}
+      for z in at.z:      
+         if z in self.species: self.species[z]+=1
+         else: self.species[z] = 1
+            
+      self.zspecies = self.species.keys();
+      self.zspecies.sort(); 
+      lspecies = 'n_species='+str(len(self.zspecies))+' species_Z={ '
+      for z in self.zspecies: lspecies = lspecies + str(z) + ' '
+      lspecies = lspecies + '}'
+   
+      at.set_cutoff(coff);
+      at.calc_connect();
+      
+      self.nenv = 0
+      for sp in self.species:
+         if sp in nocenter: 
+            self.species[sp]=0
+            continue # Option to skip some environments
+         
+         # first computes the descriptors of species that are present
+         desc = quippy.descriptors.Descriptor("soap central_weight="+str(cw)+"  covariance_sigma0=0.0 atom_sigma="+str(gs)+" cutoff="+str(coff)+" n_max="+str(nmax)+" l_max="+str(lmax)+' '+lspecies+' Z='+str(sp) )   
+         psp=np.asarray(desc.calc(at,desc.dimensions(),self.species[sp])).T;
+      
+         # now repartitions soaps in environment descriptors
+         lenv = []
+         for p in psp:
+            nenv = environ(nmax, lmax, self.alchem)
+            nenv.convert(sp, self.zspecies, p)
+            lenv.append(nenv)
+         self.env[sp] = lenv
+         self.nenv += self.species[sp]
+         
+      # adds kit data   
+      if kit is None: kit = {}
+      
+      for sp in kit:
+         if not sp in self.species: 
+            self.species[sp]=0
+            self.env[sp] = []
+         for k in range(self.species[sp], kit[sp]):
+            self.env[sp].append(environ(self.nmax,self.lmax,self.alchem,sp))
+            self.nenv+=1
+         self.species[sp] = kit[sp]          
+      
+      self.zspecies = self.species.keys();
+      self.zspecies.sort(); 
+      
+      # also compute the global (flattened) fingerprint
+      self.globenv = environ(nmax, lmax, self.alchem)
+      for k, se in self.env.items():
+         for e in se:
+            self.globenv.add(e)
+
+
+def gcd(a,b):
+   if (b>a): a,b = b, a
+   
+   while (b):  a, b = b, a%b
+   
+   return a
+   
+def lcm(a,b):
+   return a*b/gcd(b,a)
+
+def gstructk(strucA, strucB, alchem=alchemy(), periodic=False):
+    
+   return envk(strucA.globenv, strucB.globenv, alchem) 
+
+def structk(strucA, strucB, alchem=alchemy(), periodic=False, mode="sumlog", fout=None):
+   nenv = 0
+   
+   if periodic: # replicate structures to match structures of different periodicity
+      # we do not check for compatibility at this stage, just assume that the 
+      # matching will be done somehow (otherwise it would be exceedingly hard to manage in case of non-standard alchemy)
+      nspeciesA = []
+      nspeciesB = []
+      for z in strucA.zspecies:
+         nspeciesA.append( (z, strucA.getnz(z)) )
+      for z in strucB.zspecies:
+         nspeciesB.append( (z, strucB.getnz(z)) )
+      nenvA = strucA.nenv
+      nenvB = strucB.nenv      
+   else:   
+      # top up missing atoms with isolated environments
+      # first checks whic atoms are present
+      zspecies = sorted(list(set(strucB.zspecies+strucA.zspecies)))
+      nspecies = []
+      for z in zspecies:
+         nz = max(strucA.getnz(z),strucB.getnz(z))
+         nspecies.append((z,nz)) 
+         nenv += nz
+      nenvA = nenvB = nenv
+      nspeciesA = nspeciesB = nspecies
+      
+   if mode=="average":
+       genvA=strucA.globenv
+       genvB=strucB.globenv
+        
+       gk = envk(genvA, genvB, alchem)
+       
+   np.set_printoptions(linewidth=500,precision=4)
+
+   kk = np.zeros((nenvA,nenvB),float)
+   ika = 0
+   ikb = 0   
+   for za, nza in nspeciesA:
+      for ia in xrange(nza):
+         envA = strucA.getenv(za, ia)         
+         ikb = 0
+         for zb, nzb in nspeciesB:
+            acab = alchem.getpair(za,zb)
+            for ib in xrange(nzb):
+               envB = strucB.getenv(zb, ib)
+               if alchem.mu > 0 and (strucA.ismissing(za, ia) ^ strucB.ismissing(zb, ib)):
+                  if mode == "kdistance" or mode == "nkdistance":  # includes a penalty dependent on "mu", in a way that is consistent with the definition of kernel distance
+                     kk[ika,ikb] = acab - alchem.mu/2  
+                  else:
+                     kk[ika,ikb] = np.exp(-alchem.mu) * acab 
+               else:
+                  kk[ika,ikb] = envk(envA, envB, alchem) * acab
+               ikb+=1
+         ika+=1
+
+   if fout != None:
+      # prints out similarity information for the environment pairs
+      fout.write("# atomic species in the molecules (possibly topped up with dummy isolated atoms): \n")      
+      for za, nza in nspeciesA:
+         for ia in xrange(nza): fout.write(" %d " % (za) )
+      fout.write("\n");
+      for zb, nzb in nspeciesB:
+         for ib in xrange(nzb): fout.write(" %d " % (zb) )
+      fout.write("\n");
+      
+      fout.write("# environment kernel matrix: \n")      
+      for r in kk:
+         for e in r:
+            fout.write("%8.4e " % (e) )
+         fout.write("\n")
+       
+   if periodic: 
+      # now we replicate the (possibly rectangular) matrix to make
+      # a square matrix for matching
+      nenv = lcm(nenvA, nenvB)
+      skk = np.zeros((nenv, nenv), float)
+      for i in range(nenv/nenvA):
+         for j in range(nenv/nenvB):
+            skk[i*nenvA:(i+1)*nenvA,j*nenvB:(j+1)*nenvB] = kk
+      kk = skk
+      
+   # Now we have the matrix of scalar products. 
+   # We can first find the optimal scalar product kernel
+   # we must find the maximum "cost"
+   if mode == "logsum":
+      hun=best_pairs(1.0-kk)
+   
+      dotcost = 0.0
+      for pair in hun:
+         dotcost+=kk[pair[0],pair[1]]
+      cost = -np.log(dotcost/nenv)
+   elif mode == "kdistance" or mode == "nkdistance":
+      dk = 2*(1-kk)
+      hun=best_pairs(dk)
+      distcost = 0.0
+      for pair in hun:
+         distcost+=dk[pair[0],pair[1]]
+      if periodic or mode == "nkdistance":
+         # kdistance is extensive, nkdistance (or periodic matching) is intensive
+         distcost*=1.0/nenv
+      cost = distcost # this is really distance ^ 2      
+   elif mode == "sumlog":   
+      dk = (kk +1e-100)/(1+1e-100)  # avoids inf...
+      dk = -np.log(dk)
+      #print dk
+      hun=best_pairs(dk)
+      distcost = 0.0
+      for pair in hun:
+         distcost+=dk[pair[0],pair[1]]
+      if periodic:    
+         # here we normalize by number of environments only when doing 
+         # periodic matching, so that otherwise the distance is extensive
+         distcost*=1.0/nenv
+      cost = distcost
+   elif mode == "permanent":
+      from permanent import permanent
+      perm=permanent(np.array(kk,dtype=complex))       
+      return perm.real/np.math.factorial(nenv)
+   else: raise ValueError("Unknown global fingerprint mode ", mode)
+   
+   if fout != None:
+      fout.write("# optimal environment list: \n")      
+      for pair in hun:
+         fout.write("%d  %d  \n" % (pair[0],pair[1]) )
+      fout.close()
+   
+   if cost<0.0: 
+    #  print >> sys.stderr, "\n WARNING: negative squared distance ", cost, "\n"
+      return 0.0 
+   else: return np.sqrt(cost)
