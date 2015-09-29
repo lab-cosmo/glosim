2178ef5c3d29d884d57dc0ba3434aa7ee5b7df05
Sandip De <1sandipde@gmail.com>
2015-09-29 16:31:52 +0200

    Does not work yet. But I am probably going to break it even more so I save a snapshot (MC)

--- libmatch/lap/lap.py
+++ libmatch/lap/lap.py
@@ -33,36 +33,6 @@ def main1(filename):
     
     
     if mtx.shape[0] != mtx.shape[1]:
-        nmtx = np.zeros((max(mtx.shape), max(mtx.shape)), float)
-        nmtx[:] = myinf
-        for i in xrange(mtx.shape[0]):
-            for j in xrange(mtx.shape[1]):
-                nmtx[i,j] = mtx[i,j]
-    bp = best_pairs(nmtx)
-    tc = 0
-    
-    for p in bp:
-        pc = nmtx[p[0],p[1]]
-        print p, pc
-        if pc<myinf: tc += nmtx[p[0],p[1]]
-    print "total cost: ", tc
-
-def gcd(a,b):
-   if (b>a): a,b = b, a
-   
-   while (b):  a, b = b, a%b
-   
-   return a
-   
-def lcm(a,b):
-   return a*b/gcd(b,a)
-   
-def main(filename):
-    mtx=np.loadtxt(filename)
-    myinf= 1e100
-    
-    
-    if mtx.shape[0] != mtx.shape[1]:
         mm = lcm(mtx.shape[0], mtx.shape[1])
         nmtx = np.zeros((mm,mm), float)        
         for i in range(mm/mtx.shape[0]):
@@ -77,8 +47,83 @@ def main(filename):
         pc = nmtx[p[0],p[1]]
         print p, pc
         if pc<myinf: tc += nmtx[p[0],p[1]]
-    print "total cost: ", tc
-        
+    print "reference cost: ", tc
+
+def gcd(a,b):
+   if (b>a): a,b = b, a
+   
+   while (b):  a, b = b, a%b
+   
+   return a
+   
+def lcm(a,b):
+   return a*b/gcd(b,a)
+   
+def main(filename):
+    mtx=np.loadtxt(filename)
+    myinf= 1e100
+    notlast=True
+    final_cost=0.0
+    nA, nB = mtx.shape
+    elA = np.asarray(range(nA))
+    elB = np.asarray(range(nB))
+    while notlast:
+      if mtx.shape[0] > mtx.shape[1]:
+          mm = mtx.shape[0]
+          nbuf=mtx.shape[0] - mtx.shape[1]
+          nmtx = np.zeros((mm,mm), float)        
+          for i in range(mtx.shape[0]):
+              for j in range(mtx.shape[1]):
+                  nmtx[i,j]=mtx[i,j]
+              for j in range(mtx.shape[1],mtx.shape[1]+nbuf):
+                  nmtx[i,j] = myinf
+      else:
+          mm = mtx.shape[1]
+          nbuf=mtx.shape[1] - mtx.shape[0]
+          if nbuf==0: notlast=False
+          nmtx = np.zeros((mm,mm), float)
+          nmtx[:]=myinf        
+          for i in xrange(mtx.shape[0]):
+              for j in xrange(mtx.shape[1]):
+                  nmtx[i,j] = mtx[i,j]
+         
+      
+      print nmtx                  
+      bp = best_pairs(nmtx)
+      tc = 0
+      rowlist=[]
+      collist=[]
+      for p in bp:
+          pc = nmtx[p[0],p[1]]
+          print p, pc
+          if pc<myinf: tc += nmtx[p[0],p[1]]
+          else:
+             rowlist.append(p[0])
+             collist.append(p[1])
+      print "total cost: ", tc
+      
+      if mtx.shape[0] > mtx.shape[1]:
+        nold=mtx.shape[1]
+        nmtx=np.zeros((len(rowlist),nold),float)
+        for i in range(nbuf):
+          for j in range(nold):
+            nmtx[i,j]=mtx[rowlist[i],j]
+        mtx=np.zeros((len(rowlist),nold))
+        mtx=nmtx
+         
+      else:
+        nold=mtx.shape[0]
+        nmtx=np.zeros((nold,nbuf),float)
+        for i in range(nold):
+          for j in range(nbuf):
+            nmtx[i,j]=mtx[i,collist[j]]
+        mtx=np.zeros((nold,nbuf))
+        mtx=nmtx
+      final_cost+=tc
+    print "Final Cost= ", final_cost
+       
     
 if __name__ == "__main__":
+    np.set_printoptions(linewidth=1000)
+    main1(*sys.argv[1:])
     main(*sys.argv[1:])
