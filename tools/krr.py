2acb758992b41048a6590cde563b43e4320e2123
Michele Ceriotti <michele.ceriotti@gmail.com>
2016-02-22 21:41:46 +0100

    Much improved krr code including FPS training point selection and two-level test set

--- tools/krr.py
+++ tools/krr.py
@@ -4,14 +4,56 @@
 # and a vector of the observed properties. syntax:
 # $  krr.py <kernel.dat> <properties.dat> [ testfrac, csi, sigma ]
 
+import argparse
 import numpy as np
 import sys
 
-def main(kernel, prop,  testfrac="0.25", csi="2", sigma="1e-3", ntests=10):
-    testfrac=float(testfrac) 
+def segfind(cp, cs):
+    a = 0
+    b = len(cp)    
+    while (b-a)>1:
+        c = int((b+a)/2)
+        if cs<cp[c]:
+            b = c
+        else:
+            a = c
+    if cs < cp[a]:
+        return a
+    else:
+        return b
+        
+
+def randomsubset(ndata, nsel, plist=None):    
+    if nsel > ndata:
+        raise ValueError("Cannot select data out of thin air")
+    if nsel == ndata: 
+        return np.asarray(range(ndata))
+    cplist = np.zeros(ndata)
+    if plist is None:
+        plist = np.ones(ndata, float)
+        
+    # computes initial cumulative probability distr.
+    cplist[0]=plist[0] 
+    for i in xrange(1,ndata):
+        cplist[i]=cplist[i-1]+plist[i]
+    
+    rdata = np.zeros(nsel, int)
+    for i in xrange(nsel):
+        csel = np.random.uniform() * cplist[-1]
+        isel = segfind(cplist, csel)
+        rdata[i] = isel
+        psel = plist[isel]
+        for j in xrange(isel,ndata):
+            cplist[j] -= psel
+    return rdata
+
+def main(kernel, props, mode, trainfrac, csi, sigma, ntests, ttest):
+
+    trainfrac=float(trainfrac) 
     csi = float(csi)
     sigma = float(sigma)
     ntests = int(ntests)
+    ttest=float(ttest)
      
     # reads kernel
     fkernel=open(kernel, "r")
@@ -32,11 +74,10 @@ def main(kernel, prop,  testfrac="0.25", csi="2", sigma="1e-3", ntests=10):
     
     # first hyperparameter - we raise the kernel to a positive exponent to make it sharper or smoother
     kij = kij**csi
-    #print >> sys.stderr, kij[0]
     
     # reads properties
     p = np.zeros(nel)
-    fprop=open(prop,"r")
+    fprop=open(props,"r")
     fline = fprop.readline()
     while fline[0]=='#': fline=fprop.readline()
     ik=0
@@ -48,52 +89,132 @@ def main(kernel, prop,  testfrac="0.25", csi="2", sigma="1e-3", ntests=10):
         print "ERROR"
         exit()
     
-    # chooses test set randomly
+    # chooses test
     testmae=0
     trainmae=0
+    truemae=0
     testrms=0
     trainrms=0
-    ntrain=0
-    ntest=0
+    truerms=0
+    testsup=0
+    trainsup=0
+    truesup=0
+    ctrain=0
+    ctest=0
+    ctrue=0
     
     vp = np.var(p)    
 #    kij *= vp        
-    for itest in xrange(ntests):
-        ltest=[]
-        ltrain=[]
+    np.set_printoptions(threshold=10000)
+    ntrain = int(trainfrac*nel)
+    ntrue = int(ttest*nel)
+
+    for itest in xrange(ntests):        
+        ltest = np.zeros(nel-ntrain-ntrue,int)
+        ltrain = np.zeros(ntrain,int)
+        
+        # if specified, select some elements that are completely ignored from both selection and training
+        ltrue = np.zeros(ntrue, int)
+        psel = np.ones(nel,float)
+        if ntrue > 0:
+            ltrue = randomsubset(nel, ntrue)
+            psel[ltrue] = 0.0
+                        
+        if mode == "random":
+            ltrain[:] = randomsubset(nel, ntrain, psel)
+        elif mode == "fps":            
+            isel=int(np.random.uniform()*nel)
+            while isel in ltrue:
+                isel=int(np.random.uniform()*nel)
+                
+            ldist = 1e100*np.ones(nel,float)
+            imin = np.zeros(nel,int) # index of the closest FPS grid point
+            ltrain[0]=isel
+            nontrue = np.setdiff1d(range(nel), ltrue)
+            for nsel in xrange(1,ntrain):
+                dmax = 0
+                imax = 0       
+                for i in nontrue:
+#                    if i in ltrue: continue
+                    dsel = np.sqrt(1.0-kij[i, isel])
+                    if dsel < ldist[i]:
+                       imin[i] = nsel-1                    
+                       ldist[i] = dsel
+                    if ldist[i] > dmax:
+                        dmax = ldist[i]; imax = i
+                print "selected ", isel, " distance ", dmax
+                isel = imax
+                ltrain[nsel] = isel
+            
+            for i in xrange(nel):
+                if i in ltrue: continue                    
+                dsel = np.sqrt(1.0-kij[i, isel])
+                if dsel < ldist[i]:
+                    imin[i] = nsel-1
+                    ldist[i] = dsel
+                if ldist[i] > dmax:
+                    dmax = ldist[i]; imax = i
+        
+        k = 0
         for i in xrange(nel):
-            if np.random.uniform()<testfrac:  ltest.append(i)
-            else: ltrain.append(i)
-        ltest = np.asarray(ltest, int)
-        ltrain = np.asarray(ltrain, int)
-    
+            if not i in ltrain and not i in ltrue: 
+                ltest[k] = i
+                k += 1
+            
         tp = p[ltrain]
         tk = kij[ltrain][:,ltrain].copy()
+        #print lweight
         for i in xrange(len(ltrain)):
-            tk[i,i]+=sigma  # diagonal regularization
+            tk[i,i]+=sigma #/ lweight[i]  # diagonal regularization times weight!
         tc = np.linalg.solve(tk, tp)
         krp = np.dot(kij[:,ltrain],tc)   
 
         mae=abs(krp[ltest]-p[ltest]).sum()/len(ltest)
         rms=np.sqrt(((krp[ltest]-p[ltest])**2).sum()/len(ltest))
-        print "# run: %d TEST MAE: %f RMS: %f" % (itest, mae, rms)
+        sup=abs(krp[ltest]-p[ltest]).max()
+        print "# run: %d test-set MAE: %f RMS: %f SUP: %f" % (itest, mae, rms, sup)
+        
 
         testmae += abs(krp[ltest]-p[ltest]).sum()/len(ltest)
         trainmae += abs(krp[ltrain]-p[ltrain]).sum()/len(ltrain)
+        if ntrue>0: truemae += abs(krp[ltrue]-p[ltrue]).sum()/len(ltrue)
         testrms += np.sqrt(((krp[ltest]-p[ltest])**2).sum()/len(ltest))
         trainrms += np.sqrt(((krp[ltrain]-p[ltrain])**2).sum()/len(ltrain))
-        ntrain+=len(ltrain)
-        ntest+=len(ltest)
+        if ntrue>0: truerms += np.sqrt(((krp[ltrue]-p[ltrue])**2).sum()/len(ltrue))
+        testsup+=abs(krp[ltest]-p[ltest]).max()
+        trainsup+=abs(krp[ltrain]-p[ltrain]).max()
+        if ntrue>0: truesup+=abs(krp[ltrue]-p[ltrue]).max()
+        ctrain+=len(ltrain)
+        ctest+=len(ltest)
+        ctrue+=len(ltrue)
+                
+        for i in xrange(nel):
+           if i in ltrain: 
+               lab = "TRAIN "
+           elif i in ltrue:
+               lab = "TRUE "
+           else: lab = "TEST"
+           print i, p[i], krp[i], lab 
 
-    print "# KRR results (%d tests, %f training p., %f test p.): csi=%f  sigma=%f" % (ntests, ntrain/ntests, ntest/ntests, csi, sigma) 
-    print "# Train points MAE=%f  RMSE=%f" % (trainmae/ntests, trainrms/ntests)
-    print "# Test points  MAE=%f  RMSE=%f" % (testmae/ntests, testrms/ntests)
-    print "# Full KRR predictions: index target krr"
-    for i in xrange(nel):
-        print i, p[i], krp[i]
-
-    
-    
+    print "# KRR results (%d tests, %f training p., %f test p.): csi=%f  sigma=%f" % (ntests, ctrain/ntests, ctest/ntests, csi, sigma) 
+    print "# Train points MAE=%f  RMSE=%f  SUP=%f" % (trainmae/ntests, trainrms/ntests, trainsup/ntests)
+    print "# Test points  MAE=%f  RMSE=%f  SUP=%f " % (testmae/ntests, testrms/ntests, testsup/ntests)
+    if len(ltrue) > 0: 
+        print "# True test points  MAE=%f  RMSE=%f  SUP=%f " % (truemae/ntests, truerms/ntests, truesup/ntests)
     
 if __name__ == '__main__':
-   main(*sys.argv[1:])
+    parser = argparse.ArgumentParser(description="""Computes KRR and analytics based on a kernel matrix and a property vector.""")
+                           
+    parser.add_argument("kernel", nargs=1, help="Kernel matrix")      
+    parser.add_argument("props", nargs=1, help="Property file")
+    parser.add_argument("--mode", type=str, default="random", help="Train point selection (e.g. --mode random / fps / cur")      
+    parser.add_argument("-f", type=float, default='0.5', help="Train fraction")
+    parser.add_argument("--truetest", type=float, default='0.0', help="Take these points out from the selection procedure")
+    parser.add_argument("--csi", type=float, default='1.0', help="Kernel scaling")
+    parser.add_argument("--sigma", type=float, default='1e-3', help="Sigma")
+    parser.add_argument("--ntests", type=int, default='10', help="Number of tests")
+    
+    args = parser.parse_args()
+    
+    main(kernel=args.kernel[0], props=args.props[0], mode=args.mode, trainfrac=args.f, csi=args.csi, 
+         sigma=args.sigma, ntests=args.ntests, ttest=args.truetest)
