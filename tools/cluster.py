a97e37d5378c347eff4b5cace61cd2db3e89c010
sandipde <1sandipde@gmail.com>
2016-04-13 11:49:27 +0200

    updated cluster.py

--- tools/cluster.py
+++ tools/cluster.py
@@ -15,7 +15,7 @@ except:
 
 from collections import Counter
 
-def main(distmatrixfile,nclust,mode='average',proplist='',plot=False,calc_sd=False,rect_matrixfile=''):
+def main(distmatrixfile,dcut,mode='average',proplist='',plot=False,calc_sd=False,rect_matrixfile=''):
    project=False
    sim=np.loadtxt(distmatrixfile)
    if rect_matrixfile != '' :
@@ -25,13 +25,13 @@ def main(distmatrixfile,nclust,mode='average',proplist='',plot=False,calc_sd=Fal
          return
       project=True
    if proplist!='': prop=np.loadtxt(proplist)
-   Z=sc.linkage(sim,mode)
+   sim2=sim*sim
+   Z=sc.linkage(sim2,mode)
    n=len(sim)
    cdist=Z[:,2] 
-#   np.savetxt('dist.dat',cdist)
-   if nclust<1: 
-     nclust=estimate_ncluster(cdist)
-     print "Estimated ncluster:",nclust
+   np.savetxt('dist.dat',cdist)
+   nclust=estimate_ncluster(cdist,dcut)
+   print "Estimated ncluster:",nclust
    print "mean+std, cutoffdist", np.sqrt(np.var(Z[:,2]))+np.mean(Z[:,2]),(Z[n-nclust,2])
    clist=sc.fcluster(Z,nclust,criterion='maxclust')
    c_count=Counter(clist)
@@ -41,9 +41,9 @@ def main(distmatrixfile,nclust,mode='average',proplist='',plot=False,calc_sd=Fal
    structurelist=[]
    for iclust in range(1,len(c_count)+1):  #calculate mean dissimilary and pick representative structure for each cluster
       indices = [i for i, x in enumerate(clist) if x == iclust] #indices for cluster i
+      nconf=len(indices)
       structurelist.append(indices)
       sumd=0.0
-      icount=0
       kurt=0.0
       if proplist!='': kurt= kurtosis(prop[indices],fisher=True)
       #calculate mean dissimilarity in each group
@@ -51,12 +51,10 @@ def main(distmatrixfile,nclust,mode='average',proplist='',plot=False,calc_sd=Fal
          ind1=indices[iconf]
          for jconf in range(len(indices)):
            ind2=indices[jconf]
-           sumd+=sim[ind1][ind2]
-           icount+=1
-      meand=sumd/icount
+           sumd+=sim2[ind1][ind2]
+      meand=np.sqrt(sumd/(nconf*nconf))
       
       # pick the configuration with min variance in the group
-      icount=0
       minvar=9999
       var=0.0
       for iconf in range(len(indices)):
@@ -64,16 +62,15 @@ def main(distmatrixfile,nclust,mode='average',proplist='',plot=False,calc_sd=Fal
         ind1=indices[iconf]
         for jconf in range(len(indices)):
           ind2=indices[jconf]
-          ivar+=(sim[ind1][ind2]-meand)**2
-        ivar=ivar/max(1,(len(indices)-1))
+          ivar+=(sim2[ind1][ind2]-meand**2)**2
+        ivar=ivar/nconf
         var+=ivar  
-        icount+=1      
         if(ivar<minvar):  
           minvar=ivar
           iselect=ind1
-      var=var/max(1,icount)  
+      var=var/nconf  
       rep_ind.append(iselect)
-      print len(indices), meand , np.sqrt(var) , iselect ,kurt
+      print nconf, meand , np.sqrt(var) , iselect ,kurt
 #   print rep_ind
    print "index of clusters"
    for i in structurelist:
@@ -96,27 +93,56 @@ def main(distmatrixfile,nclust,mode='average',proplist='',plot=False,calc_sd=Fal
         if i in project_rep: iselect=2
         f.write("%d   %d \n " %(project_groupid[i],  iselect)) 
      f.close()
-   if plot: plotdendro(Z,nclust)
    if (calc_sd):
-     filename=mode+'-sd.dat'
+     filename= filename=basename(distmatrixfile)+'-cluster-sd.dat'
      f=open(filename,"w")
      f.write("dist_sd ")
      if proplist!='':f.write("prop_sd ")
+     f.write("representative config")
      f.write("\n")
-     sim_sd=dissimilarity_sd(Z,sim) 
+     sim_sd,rep_index=dissimilarity_sd(Z,sim2) 
      if proplist!='': psd=prop_sd(Z,prop)
      for i in range(len(Z)):
          f.write("%f" %(sim_sd[i]))
          if proplist!='':f.write("   %f" %(psd[i]))
+         f.write("  %d" %(rep_index[i]))
          f.write("\n")
+   if plot: 
+        filename=basename(distmatrixfile)+'-dendogram.eps'
+        plotdendro(Z,nclust,filename,rep_ind)
 
-def plotdendro(Z,ncluster):
-  plt.figure(figsize=(25, 10))
+def plotdendro(Z,ncluster,filename,rep_ind):
+  plt.figure(figsize=(10, 15))
   plt.title('Hierarchical Clustering Dendrogram')
   plt.xlabel('sample index')
   plt.ylabel('distance')
-  sc.dendrogram(Z,truncate_mode='lastp', p=ncluster,leaf_rotation=90.,leaf_font_size=8.,show_contracted=True)
-  plt.show()
+  d=sc.dendrogram(Z,truncate_mode='lastp', p=ncluster,orientation='right',leaf_rotation=90.,leaf_font_size=20.,show_contracted=False)
+#  coord = np.c_[np.array(d['icoord'])[:,1:3],np.array(d['dcoord'])[:,1]]
+#  coord = coord[np.argsort(coord[:,2])]
+  num=ncluster-1
+  coord=[]
+  for i in range(len(d['icoord'])):
+    if d['dcoord'][i][0]==0.0 :
+     coord.append(d['icoord'][i][0])
+  for i in range(len(d['icoord'])):
+    if d['dcoord'][i][3]==0.0 :
+     coord.append(d['icoord'][i][3])
+  #print d['leaves']
+  #return
+  #for posi in coord:
+  # x = posi
+  #  y = 0.05
+  #  plt.plot(x, y, 'ro')
+  #  plt.annotate("%2i" % rep_ind[num], (x, y), xytext=(0, -8),
+  #               textcoords='offset points',
+  #               va='top', ha='center')
+  #  num = num-1
+  #plt.show()
+  
+  plt.savefig(filename, dpi=100, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype='letter', format=None,
+        transparent=True, bbox_inches=None, pad_inches=0.1,
+        frameon=None)
       
 def project_config(clusterlist,rect_matrix,rep_ind):
   nland=len(rect_matrix[0])
@@ -146,6 +172,7 @@ def dissimilarity_sd(Z,sim):
   clusterlist=[]
   ncluster=0
   sdlist=[]
+  rep_index=[]
   for i in range(len(Z)):
     id1=int(Z[i,0])
     id2=int(Z[i,1])
@@ -192,26 +219,40 @@ def dissimilarity_sd(Z,sim):
         if(ivar<minvar):
             minvar=ivar
             iselect=ind1
+    rep_index.append(iselect)
     var=var/(icount)
     sd=np.sqrt(var)
     sdlist.append(sd)
-  return sdlist
+  return sdlist,rep_index
 #    print len(clusterlist[i]),meand,var,sd,iselect,Z[i,2]
 #  print "clusters:", nl-elbow+2
 
-def estimate_ncluster(dist):
+def estimate_ncluster(dist,dcut):
   n=len(dist)
-  b=[n-1,dist[n-1]-dist[0]]
-  b=np.array(b)
-  b=b/np.linalg.norm(b)
-  dmax=0.0
-  for i in range(n):
-    p=[n-1-i,dist[n-1]-dist[i]]
-    d=np.linalg.norm(p-np.dot(p,b)*b)
-    if d>dmax :
-       elbow=i
-       dmax=d
-  return int((n-elbow)*0.5)
+  if dcut>0.0 : 
+    for i in range(n):
+      if dist[i]>dcut : 
+          nclust=n-i
+          break
+
+  else:
+    b=[n-1,dist[n-1]-dist[0]]
+    b=np.array(b)
+    b=b/np.linalg.norm(b)
+    dmax=0.0
+    for i in range(n):
+      p=[n-1-i,dist[n-1]-dist[i]]
+      d=np.linalg.norm(p-np.dot(p,b)*b)
+      if d>dmax :
+         elbow=i
+         dmax=d
+    dcut=dist[elbow]*1.2
+    print "estimated dcut=",dcut
+    for j in range(n):
+      if dist[j]>dcut : 
+            nclust=n-j
+            break
+  return nclust
 
 
 def prop_sd(Z,prop):
@@ -255,12 +296,12 @@ if __name__ == '__main__':
 
     parser.add_argument("sim", nargs=1, help="Kernel matrix")
     parser.add_argument("--mode", type=str, default="average", help="Linkage mode (e.g. --mode average/single/complete/median/centroid")
-    parser.add_argument("--nclust", type=int, default='0', help="Number of clusters")
+    parser.add_argument("--dcut", type=float, default='0', help="distance cutoff to cut the dendrogram. if dcut=0 then it is autamaticlly estimated")
     parser.add_argument("--prop", type=str, default='', help="property file")
     parser.add_argument("--plot",  action="store_true", help="Plot the dendrogram")
     parser.add_argument("--calc_sd",  action="store_true", help="calculate standard div of the dist and prop for all level of clustering")
     parser.add_argument("--project",  type=str,default='', help="Project configurations using Rect Dist Matrix file")
 
     args = parser.parse_args()
-    main(args.sim[0],args.nclust,mode=args.mode,proplist=args.prop,plot=args.plot,calc_sd=args.calc_sd,rect_matrixfile=args.project)
+    main(args.sim[0],args.dcut,mode=args.mode,proplist=args.prop,plot=args.plot,calc_sd=args.calc_sd,rect_matrixfile=args.project)
 
