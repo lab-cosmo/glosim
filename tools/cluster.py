Fixed --mathematica option of cluster.py (indexing) --dcut has to be negative to trigger automatic calculation.

tools/cluster.py
@@ -27,7 +27,7 @@ def main(distmatrixfile,dcut,mode='average',proplist='',plot=False,calc_sd=False
    if proplist!='': prop=np.loadtxt(proplist)
    sim2=sim*sim
    Z=sc.linkage(sim2,mode)
-   if mtcia : mathematica_cluster(Z,sim)
+   if mtcia : mathematica_cluster(Z,sim,'cluster-mathematica.dat')
    n=len(sim)
    cdist=Z[:,2]
    if verbose : 
@@ -176,7 +176,8 @@ def project_config(clusterlist,rect_matrix,rep_ind):
       iselect=np.argmin(rect_matrix[:,iconfig])               
       project_rep.append(iselect)
   return(groupid,project_rep)
-def mathematica_cluster(Z,sim):
+
+def mathematica_cluster(Z,sim,fname):
    n=len(sim)
    clusterlist=[]
    nlist=[]
@@ -184,8 +185,8 @@ def mathematica_cluster(Z,sim):
     id1=int(Z[i,0])
     id2=int(Z[i,1])
     if((id1 < n) and (id2<n)):  # when two configurations are merged note their index
-       #clusterlist.append('Cluster'+str([id1,id2,Z[i,2],1,1]))
-       clusterlist.append([id1,id2,Z[i,2],1,1])
+       # in mathematica cluster index should start from 1 so '+1'
+       clusterlist.append([int(id1+1),int(id2+1),'{:.8f}'.format(Z[i,2]),1,1])
        nlist.append(2)
        #ncluster+=1
     else:
@@ -197,7 +198,7 @@ def mathematica_cluster(Z,sim):
         cl.append(clusterlist[icluster])
         n1=nlist[icluster]
       else:
-        cl.append(id1)
+        cl.append(id1+1)
         n1=1
       if id2>=n: # same logic as before
         icluster=int(id2)-n
@@ -205,9 +206,9 @@ def mathematica_cluster(Z,sim):
         cl.append(clusterlist[icluster])
         n2=nlist[icluster]
       else:
-        cl.append(id2)
+        cl.append(id2+1)
         n2=1
-      cl.append(Z[i,2])
+      cl.append('{:.8f}'.format(Z[i,2]))
       cl.append(n1)
       cl.append(n2)
    #   tmp='Cluster'+str(cl)
@@ -215,12 +216,20 @@ def mathematica_cluster(Z,sim):
    #   clusterlist.append(tmp)
       clusterlist.append(cl)
       nlist.append(n1+n2)
-   f=open('cluster-mathematica.dat','w')
-#   clusterlist[n-2].replace("'","")
-#   clusterlist[n-2].replace("\\","")
-   f.write("%s" %str(clusterlist[n-2]))
-   f.close()
-   return  
+   
+   # get the final nested cluster structure and put
+   # the mathematica Cluster statement
+   clusterliststr = str(clusterlist[n-2])
+   clusterliststr = clusterliststr.replace("[","Cluster[")
+   clusterliststr = clusterliststr.replace("'","")
+   # print a.replace("[","Cluster[")
+   fmathematica=open(fname,'w')
+   fmathematica.write(clusterliststr)
+   fmathematica.close()
+
+   return
+
+
 def dissimilarity_sd(Z,sim):
   n=len(sim)
   clusterlist=[]
@@ -283,7 +292,7 @@ def dissimilarity_sd(Z,sim):
 
 def estimate_ncluster(dist,dcut):
   n=len(dist)
-  if dcut>0.0 : 
+  if dcut>=0.0 : 
     for i in range(n):
       if dist[i]>dcut : 
           nclust=n-i

tools/env_clust.py
@@ -4,6 +4,7 @@ import numpy as np
 import quippy
 import sys
 import argparse
+from scipy.cluster import hierarchy as sc
 import matplotlib.pyplot as plt
 
 def main(fnamexyz, cutoffdist, fnamesim, zenv, nenv, dcut, selectgroupxyz2print=[], dcutisgood=False, isploting=False ):
@@ -14,10 +15,16 @@ def main(fnamexyz, cutoffdist, fnamesim, zenv, nenv, dcut, selectgroupxyz2print=
 	# Generate the file name for the new dist matrices that are without dummy atoms
 	nsim = len(nenv)
 	fname = []
+	fnamecorr = []
+	fnamemathematica = []
 	cnt = 0
 	for fn in fnamesim:
 		target = '_'+str(nenv[cnt])
-		fname.append(fn[:fn.find(target)])
+		fname.append(fn[:fn.find(target)]+'.sim')
+		fnamecorr.append(fn[:fn.find(target)]+'.corrmt')
+		fnamemathematica.append(fn[:fn.find(target)]+'.mathematica-cluster.dat')
+		cnt+=1
+		print (fn[:fn.find(target)])
 	
 	print >> sys.stderr, "Reading input file", fnamexyz
 	mols = quippy.AtomsList(fnamexyz);
@@ -28,27 +35,43 @@ def main(fnamexyz, cutoffdist, fnamesim, zenv, nenv, dcut, selectgroupxyz2print=
 	nbclst = []
 	clusterlist = []
 	nspiecies = []
+	Z = []
+	dendo = []
 
 	for it in range(nsim):
 		print '############'
 		print "Read distance matrix "+ fnamesim[it] + " with dummy atoms"
 		olddistmatrix = np.loadtxt(fnamesim[it])
+		
+		fsim = open(fnamesim[it],'r')
+		head = fsim.readline()
+		head = head.strip('#');head = head.strip('\n')
+		# print '##########' , head
+		fsim.close()
 
 		# Removes dummy atoms rows/columns from distance matrix
-		newdistmatrix, dummylist = env_corr.rmdummyfromsim(fnamexyz,olddistmatrix,zenv[it],nenv[it])
-		print len(newdistmatrix),' Real ',zenv[it],' atom '
+		newdistmatrix1, dummylist = env_corr.rmdummyfromsim(fnamexyz,olddistmatrix,zenv[it],nenv[it])
+		print len(newdistmatrix1),' Real ',zenv[it],' atom '
 		print 'Removes : ', len(dummylist),' rows/colomns from distmatrix'
-		print newdistmatrix.shape
+		print newdistmatrix1.shape
 		
+		# Write the distmatrix without dummy atoms
+		np.savetxt(fname[it],newdistmatrix1, header=head )
+
 		# get list of the cluster groups idx (same order as in dist mat) 
-		clist1 = env_corr.clusterdistmat(fname[it],newdistmatrix,dcut[it],mode='average',plot=plotdendo)
-		
+		clist1,Z1 = env_corr.clusterdistmat(fname[it],newdistmatrix1,0.,mode='average',plot=plotdendo)
+		dendo1 = sc.dendrogram(Z1)
+		# Write mathematica cluster structure
+		cluster.mathematica_cluster(Z1, newdistmatrix1,fnamemathematica[it])
+
 		# get nb of cluster groups
 		nbclst1 = len(np.unique(clist1))
 		# print a==nbclst1
-		distmatrix.append(newdistmatrix)
+		distmatrix.append(newdistmatrix1)
 		clist.append(clist1)
 		nbclst.append(nbclst1)
+		Z.append(Z1)
+		dendo.append(dendo1)
 
 		# Link the cluster groups with atom's frame and respective position in it
 		if dcutisgood:
@@ -68,12 +91,27 @@ def main(fnamexyz, cutoffdist, fnamesim, zenv, nenv, dcut, selectgroupxyz2print=
 				for i in range(1,nsim+1):
 					selectgroupxyz2print.append([]) 
 			
-			corrmtx1, corr1  = env_corr.getcorrofcluster(mols,cutoffdist,fnamexyz,clusterlist[0],clusterlist[it],nbclst[0],nbclst[it],zenv[0],zenv[it],nspiecies[0],nspiecies[it],selectgroupxyz2print[it-1])
-			corrmtx.append(corrmtx1)
+			corrmtx1, corr1,idx1, idx2  = env_corr.getcorrofcluster(mols,cutoffdist,fnamexyz,clusterlist[0],clusterlist[it],nbclst[0],nbclst[it],zenv[0],zenv[it],nspiecies[0],nspiecies[it],selectgroupxyz2print[it-1])
+			
+			# idx1 = dendo[0]['leaves']
+			# idx2 = dendo[it]['leaves']
+			print len(idx1),len(idx2)
+			print corrmtx1.shape
+			# corrmtx1 = corrmtx1[idx1,:]
+			# corrmtx1 = corrmtx1[:,idx2]
 
+			corrmtx.append(corrmtx1)
+			head1 = 'From '+fnamexyz+'  Correlation matrix between environment of atom '+str(zenv[0])+' and '+str(zenv[it])
 			print '############'
-			print 'Correlation matrix between atom ',zenv[0],' and ',zenv[it]
-			print corrmtx1
+			print head1
+			head2 = 'From '+fnamexyz+'  Dendogram ordering of atom '+str(zenv[0])
+			head3 = 'From '+fnamexyz+'  Dendogram ordering of atom '+str(zenv[it])
+
+			np.savetxt(fnamecorr[it], corrmtx1, header=head1 )
+			np.savetxt(fnamecorr[it]+str(zenv[0]), idx1, header=head2 )
+			np.savetxt(fnamecorr[it]+str(zenv[it]), idx2, header=head3 )
+
+			# print corrmtx1
 
 	if pyplot:
 		plt.show()

tools/env_corr.py
@@ -22,7 +22,8 @@ def getcorrofcluster(mols,cutoffdist,fnamexyz,clusterlist1,clusterlist2,ng1,ng2,
 	
 	# initialize correlation matrix 
 	corrmtx = np.zeros((ng1,ng2), dtype=int)  
-	
+	frame1 = np.zeros((ng1), dtype=int) 
+	frame2 = np.zeros((ng2), dtype=int) 
 	# give correlation data structured with specie of clusterlist1 
  	# as main element
  	corr1 = {x: [] for x in xrange(ng1)}
@@ -55,23 +56,22 @@ def getcorrofcluster(mols,cutoffdist,fnamexyz,clusterlist1,clusterlist2,ng1,ng2,
 	 		r1[0] = mol[clusterlist1[iframe][atm1][2]].x
 	 		r1[1] = mol[clusterlist1[iframe][atm1][2]].y
 	 		r1[2] = mol[clusterlist1[iframe][atm1][2]].z
-	 		# print r1
-	 		# print 'Frame : ', iframe, ' and atm : ', atm1
+	 		
 	 		for atm2 in xrange(nspiecies2[iframe]):
 	 			# position of the atom from clusterlist2
-	 			# print nspiecies1[iframe]
-	 			# print clusterlist2[iframe]
-	 			# print clusterlist2[iframe][atm2]
-	 			# print clusterlist2[iframe][atm2][2]
 	 			r2[0] = mol[clusterlist2[iframe][atm2][2]].x
 	 			r2[1] = mol[clusterlist2[iframe][atm2][2]].y
 	 			r2[2] = mol[clusterlist2[iframe][atm2][2]].z
 	 			
+	 			frame1[clusterlist1[iframe][atm1][0]] = clusterlist1[iframe][atm1][1]
+	 			frame2[clusterlist2[iframe][atm2][0]] = clusterlist2[iframe][atm2][1]
+
 	 			dist = np.linalg.norm(r1-r2)
 	 			# if atom is within cutoff distance then update correlation matrix
 	 			if dist < cutoffdist: 
 	 				corrmtx[clusterlist1[iframe][atm1][0],clusterlist2[iframe][atm2][0]] += 1 
 	 				corr1[clusterlist1[iframe][atm1][0]][atmcount[clusterlist1[iframe][atm1][0]]][2][zenv2][clusterlist2[iframe][atm2][0]].append(clusterlist2[iframe][atm2][2])
+	 				
 	 				# Print xyz of selected group
 					if len(selectgroupxyz2print) > 0 :
 						if 	(int(clusterlist1[iframe][atm1][0]) == selectgroupxyz2print[0] and int(clusterlist2[iframe][atm2][0]) == selectgroupxyz2print[1]):
@@ -106,7 +106,7 @@ def getcorrofcluster(mols,cutoffdist,fnamexyz,clusterlist1,clusterlist2,ng1,ng2,
 	# 	print corr1[0][i][2][zenv2]
 	# print corrmtx 
 	# print arr
-	return corrmtx, corr1 
+	return corrmtx, corr1, frame1, frame2
 
 def rmdummyfromsim(fnamexyz,distmatrix,zenv,nenv):
 	# zenv is atomic nb of env and nenv is the number of 
@@ -183,6 +183,32 @@ def linkgroup2atmidx(mols,clist,zenv,nenv):
 
 	return clusterlist, nspecies 
 
+def clusterdistmatfull(distmatrixfile,sim,mode='average',plot=False):
+	# Compute the clusturing on dist^2 so that the average 
+	# distance of a cluster with an other is the RMS distance
+	sim2 = sim*sim
+	Z = sc.linkage(sim2,mode)
+
+	# get the full tree
+	plt.figure(figsize=(10, 15))
+	plt.title('Hierarchical Clustering Dendrogram')
+	plt.xlabel('sample index')
+	plt.ylabel('distance')
+	dendo = sc.dendrogram(Z,orientation='right',leaf_rotation=90.,leaf_font_size=20.,show_contracted=False)
+	c_list = np.array(dendo['leaves'])
+
+	c_count = Counter(c_list)
+	nbclst = len(c_count)
+
+	print "Number of clusters", nbclst 
+	
+	# c_list = np.zeros(len(sim))
+
+	# # Change cluster groups numbering to (0:n-1)
+	# for i in range(len(sim)):
+	# 	c_list[i] = int(clist[i]-1)
+
+	return c_list,Z
 
 def clusterdistmat(distmatrixfile,sim,dcut,mode='average',plot=False):
 	# Compute the clusturing on dist^2 so that the average 
@@ -191,7 +217,9 @@ def clusterdistmat(distmatrixfile,sim,dcut,mode='average',plot=False):
 	Z = sc.linkage(sim2,mode)
 
 	cdist = Z[:,2]
-
+	# get the full tree
+	# dendo = sc.dendrogram(Z)
+	# clist = dendo['leaves']
    	nclust = cluster.estimate_ncluster(cdist,dcut)
 
 	clist = sc.fcluster(Z,nclust,criterion='maxclust')
@@ -222,7 +250,7 @@ def clusterdistmat(distmatrixfile,sim,dcut,mode='average',plot=False):
 	for i in range(len(sim)):
 		c_list[i] = int(clist[i]-1)
 
-	return c_list
+	return c_list,Z
 
 # Determine the representative element of each cluster group
 def getrep_ind(sim2,clist,c_count):