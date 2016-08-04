3b718237abae1b166e8d499b2bae87686d41a680
Sandip De <1sandipde@gmail.com>
2016-08-04 11:06:43 +0200

    modified krr-test.py so that we don't store the whole OOS matrix in the memory

--- tools/krr-test.py
+++ tools/krr-test.py
@@ -4,18 +4,33 @@ import numpy as np
 import math
 def main( kernel, trainvector, proplist,csi ):
        csi=float(csi)
-       kij=np.loadtxt(kernel)
+      # kij=np.loadtxt(kernel)
        tc=np.loadtxt(trainvector)
        p=np.loadtxt(proplist)
-       kij = kij**csi
+      # kij = kij**csi
        #print lweight
-       if (len(kij[0]) != len(tc)):
-         print "inconsistent kernel and train vector file"
-         return
-       if (len(kij) != len(p)):
-         print "inconsistent kernel and property file"
-         return
-       krp = np.dot(kij,tc)
+       krp=[]
+       with open(kernel) as f:
+        last_pos = f.tell()
+        header=f.readline().strip()
+        if not header.startswith("#"): f.seek(last_pos)
+        for ln in f:
+          ai=[float(x) for x in ln.split()]
+          if (len(ai) != len(tc)):
+            print "inconsistent kernel and train vector file"
+            return
+          t=0.0
+          for j in range(len(tc)):
+             t+=(ai[j]**csi)*tc[j]
+          krp.append(t)
+
+      # if (len(kij[0]) != len(tc)):
+      #   print "inconsistent kernel and train vector file"
+      #   return
+      # if (len(kij) != len(p)):
+      #   print "inconsistent kernel and property file"
+      #   return
+       #krp = np.dot(kij,tc)
        nconf=len(p)
        mae=abs(krp[:]-p[:]).sum()/nconf
        rms=np.sqrt(((krp[:]-p[:])**2).sum()/nconf)
